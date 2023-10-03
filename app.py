efbf8b648c6a7f58ae6f3cb62fb4a8f47a9c16ec
Justin Huynh <justinhuynh639@gmail.com>
2023-10-02 17:02:56 -0700

    started site connectivity checker

---
+++ app.py
@@ -0,0 +1,10 @@
+#use urllib.request to get the data from the url
+#write a function that takes a url
+#returns a response
+
+#import urllib
+import urllib.request as urllib
+
+def main(url):
+    print("This is a site connectivity checker program")
+    print("Input the url of the site")
