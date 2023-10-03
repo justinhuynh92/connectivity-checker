160ec99a6984050128c31ce24a56e004f2aa760c
Justin Huynh <justinhuynh639@gmail.com>
2023-10-02 17:12:30 -0700

    finished site connectivity checker

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-#use urllib.request to get the data from the url
-#returns a response
-
 #import urllib
 import urllib.request as urllib
-
-print("This is a site connectivity checker program")
-input_url = input("Input the url of the site: ")
 
 #write a function that takes a url
 def main(url):
     print("Checking connectivity ")
 
-    #open up url
+    #use urllib.request to get the data from the url
     response = urllib.urlopen(url)
     print("Connected to", url, "successfully")
+    print("The response code was:", response.getcode())
+
+print("This is a site connectivity checker program")
+input_url = input("Input the url of the site: ")
+
+#return a response
+main(input_url)
